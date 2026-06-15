4e7b1afa34488a44883664954a0c5f8ce3c858ff
adirut2532-alt <adirut2532@gmail.com>
2026-06-15 13:14:13 +0700

    Delete files 5 directory

--- files 5/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    // babel-preset-expo auto-configures the Reanimated/Worklets Babel plugin
-    // (SDK 50+). Do NOT add 'react-native-reanimated/plugin' manually — on
-    // Reanimated 4 that moved into react-native-worklets and adding it errors.
-    presets: ['babel-preset-expo'],
-  };
-};
