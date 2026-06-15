a84a40cc446b4cd89f49bd35674d9bbe674efc14
adirut2532-alt <adirut2532@gmail.com>
2026-06-15 15:13:17 +0700

    Update game.ts

--- game.ts
+++ game.ts
@@ -3,7 +3,8 @@
  */
 
 export type Suit = 'S' | 'H' | 'D' | 'C'; // Spades, Hearts, Diamonds, Clubs
-export type Rank = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
+export type Rank = number;
+
 
 
 
